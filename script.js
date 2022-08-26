fc8e6a35ba97d8b09c5af11f5d02eb3ac3dcdccb
Tanush Sethi <tanushsethi55@gmail.com>
2022-08-27 02:35:41 +0530

    added key support

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+const map = {
+    "1":"clap",
+    "2":"hihat",
+    "3":"kick",
+    "4":"openhat",
+    "5":"boom",
+    "6":"ride",
+    "7":"snare",
+    "8":"tom",
+    "9":"tink"
+}
+
 function playSound(event){
-    var audioName = "sounds/01 - JavaScript Drum Kit_sounds_" + this.dataset.name + ".wav";
+    console.log(event.key);
+    var audioName;
+    if(event.type == "click"){
+        audioName = "sounds/01 - JavaScript Drum Kit_sounds_" + this.dataset.name + ".wav";
+    }else if(event.keyCode>= 49 && event.keyCode<= 57){
+        audioName = "sounds/01 - JavaScript Drum Kit_sounds_" + map[event.key] + ".wav"
+    }else{
+        return;
+    }
+
     var audio = new Audio(audioName);
     audio.play();
 }
@@ -8,3 +29,5 @@ var insDivs = document.getElementsByClassName("insBox");
 for(var i = 0; i<insDivs.length ; i++){
     insDivs[i].addEventListener('click', playSound);
 }
+
+document.addEventListener("keydown",playSound);
